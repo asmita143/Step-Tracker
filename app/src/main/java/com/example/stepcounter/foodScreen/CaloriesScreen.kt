2995fa80f5d94f0a3d7a06e968ea49769270ef7a
asmita143 <89544722+asmita143@users.noreply.github.com>
2023-10-04 14:08:35 +0300

    Changes to the buttons of cloriesScreen

--- app/src/main/java/com/example/stepcounter/foodScreen/CaloriesScreen.kt
+++ app/src/main/java/com/example/stepcounter/foodScreen/CaloriesScreen.kt
@@ -1,5 +1,6 @@
 package com.example.stepcounter.foodScreen
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -8,23 +9,37 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.stepcounter.R
+import com.example.stepcounter.ui.theme.md_theme_light_background
 
 @Preview
 @Composable
 fun CaloriesScreen() {
+
     val configuration = LocalConfiguration.current
     val screenHeight = configuration.screenHeightDp.dp
 
@@ -32,7 +47,7 @@ fun CaloriesScreen() {
         modifier = Modifier
             .fillMaxHeight()
             .padding(20.dp)
-            //.background(MaterialTheme.colorScheme.background)
+            .background(colorResource(id = R.color.background))
             )
                 {
         Column(modifier = Modifier
@@ -41,19 +56,27 @@ fun CaloriesScreen() {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .border(1.dp, Color.Red)
+
             ) {
                 Row(modifier = Modifier.padding(20.dp)) {
                     Column {
                         Text(text = "Today")
+
                         Text(text = "18 February")
                     }
                 }
             }
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(height = 4.dp)
+                    .background(color = Color.Blue)
+            )
             //this box is for the calorie scale
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+
             ) {
                 Row(modifier = Modifier.padding(20.dp)) {
                     Column {
@@ -68,14 +91,17 @@ fun CaloriesScreen() {
 
         Row(modifier = Modifier
             .weight(1f, false)
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.inversePrimary)) {
+            .fillMaxWidth(),
+            ) {
             Column() {
-                Button(onClick = {}, modifier = Modifier
-                    .fillMaxWidth()) {
+                ElevatedButton(
+                    onClick = {},
+                    modifier = Modifier
+                    .fillMaxWidth()
+                    ,) {
                     Text(text = "More Info")
                 }
-                Button(onClick = {}, modifier = Modifier
+                ElevatedButton(onClick = {}, modifier = Modifier
                     .fillMaxWidth()) {
                     Text(text = "Eaten Today")
                 }
